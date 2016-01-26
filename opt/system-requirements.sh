8eaa853017bff7463468b4f8546e2f1c6a896031
João Ricardo <joaoricardo000@gmail.com>
2016-01-26 18:44:04 -0300

    Requirements update

--- opt/system-requirements.sh
+++ opt/system-requirements.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-sudo apt-get install libjpeg-dev zlib1g-dev
+sudo apt-get install -y libjpeg-dev zlib1g-dev
 sudo ln -s /usr/lib/x86_64-linux-gnu/libjpeg.so /usr/lib
 sudo ln -s /usr/lib/x86_64-linux-gnu/libfreetype.so /usr/lib
 sudo ln -s /usr/lib/x86_64-linux-gnu/libz.so /usr/lib
 sudo npm -g install pageres-cli
+sudo apt-get install -y espeak mbrola mbrola-br1 mbrola-br3 mbrola-us2

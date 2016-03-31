43b129b584501a406b159eae0b3e1030f36213b0
João Ricardo <joaoricardo000@gmail.com>
2016-03-31 10:19:01 -0300

    Update patch.sh

--- opt/patch.sh
+++ opt/patch.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 # Patch for group messages error https://github.com/tgalal/yowsup/pull/1364
-sed -i '106s/:/ and not "-" in node["to"]:/' /venv/local/lib/python2.7/site-packages/yowsup/layers/axolotl/layer.py
+# sed -i '106s/:/ and not "-" in node["to"]:/' /venv/local/lib/python2.7/site-packages/yowsup/layers/axolotl/layer.py
+# no patches
